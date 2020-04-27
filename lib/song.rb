acb62be01bb8d782f804d9489af9a9963ef23759
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-27 18:08:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,20 @@
 class Song 
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist, :genre 
   
+  @@all = []
+ 
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
   
 end
